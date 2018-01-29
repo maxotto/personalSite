18407472cbb3b743f7187f33689f24f234d42a7e
maxotto <agmfromita@mail.ru>
2018-01-29 17:53:12 +0300

    Temperature Monitor

---
+++ www/src/app/pages/temp-monitor/temp-monitor-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TempMonitorComponent } from './temp-monitor.component';
+
+const routes: Routes = [
+    { path: '', component: TempMonitorComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TempMonitorRoutingModule { }
---
+++ www/src/app/pages/temp-monitor/temp-monitor.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TempMonitorComponent } from './temp-monitor.component';
+
+describe('TempMonitorComponent', () => {
+  let component: TempMonitorComponent;
+  let fixture: ComponentFixture<TempMonitorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TempMonitorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TempMonitorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
