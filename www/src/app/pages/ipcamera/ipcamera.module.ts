73e44757f642ad048227745374e6c8a65758f2ff
maxotto <agmfromita@mail.ru>
2018-03-02 11:27:30 +0300

    Picture on main site

--- www/src/app/pages/ipcamera/ipcamera.module.ts
+++ www/src/app/pages/ipcamera/ipcamera.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { IpcameraComponent } from './ipcamera.component';
+import {IpcameraRoutingModule} from './ipcamera-routing.module';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    IpcameraRoutingModule,
+    NgbModule
   ],
   declarations: [IpcameraComponent]
 })
