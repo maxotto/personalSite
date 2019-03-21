cfcc127afb1cb87f934633435690d76ed1395566
Andrey <andrey@mikhailichenko.su>
2019-03-21 15:44:56 +0300

    get changes from last working place

--- www/src/app/app-routing.module.ts
+++ www/src/app/app-routing.module.ts
@@ -5,16 +5,16 @@ import { HomeComponent} from './components/home/home.component';
 import { NotFoundComponent} from './components/not-found/not-found.component';
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'login', loadChildren: './pages/login/login.module#LoginModule' },
-    { path: 'map', loadChildren: './pages/google-maps/google-maps.module#GoogleMapsModule', canLoad: [AuthGuard] , canActivate: [AuthGuard] },
-    { path: 'about', loadChildren: './pages/about/about.module#AboutModule'},
-    { path: 'gallery', loadChildren: './pages/photo-gallery/photo-gallery.module#PhotoGalleryModule', canLoad: [AuthGuard]  },
-    { path: 'tmonitor', loadChildren: './pages/temp-monitor/temp-monitor.module#TempMonitorModule', canLoad: [AuthGuard]  },
-    { path: 'ipcamera', loadChildren: './pages/ipcamera/ipcamera.module#IpcameraModule', canLoad: [AuthGuard]  },
-    { path: 'not-found', component: NotFoundComponent},
-    { path: '**', redirectTo: 'not-found' }
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginModule' },
+  { path: 'map', loadChildren: './pages/google-maps/google-maps.module#GoogleMapsModule', canLoad: [AuthGuard] , canActivate: [AuthGuard] },
+  { path: 'about', loadChildren: './pages/about/about.module#AboutModule'},
+  { path: 'gallery', loadChildren: './pages/photo-gallery/photo-gallery.module#PhotoGalleryModule', canLoad: [AuthGuard]  },
+  { path: 'tmonitor', loadChildren: './pages/temp-monitor/temp-monitor.module#TempMonitorModule', canLoad: [AuthGuard]  },
+  { path: 'ipcamera', loadChildren: './pages/ipcamera/ipcamera.module#IpcameraModule', canLoad: [AuthGuard]  },
+  { path: 'not-found', component: NotFoundComponent},
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
